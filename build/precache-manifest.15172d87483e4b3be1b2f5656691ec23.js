60410816b0161386412e4cf40f1aaffd9eabb035
yudanZ <zhai.yudan@gmail.com>
2020-09-09 20:47:34 +0200

    update

--- build/precache-manifest.15172d87483e4b3be1b2f5656691ec23.js
+++ build/precache-manifest.15172d87483e4b3be1b2f5656691ec23.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "65c94916c784044e9c360b00f6b97082",
+    "revision": "2f8e3722c512fcd9b2f33c6a16401b33",
     "url": "/robotFriends/index.html"
   },
   {
-    "revision": "eec361b0d950f34048ff",
+    "revision": "1645b9443946df853b25",
     "url": "/robotFriends/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "505c8a500e23440eee91",
+    "revision": "f83495740799f9201a28",
     "url": "/robotFriends/static/css/main.9bcfdf2f.chunk.css"
   },
   {
-    "revision": "eec361b0d950f34048ff",
-    "url": "/robotFriends/static/js/2.7e91a854.chunk.js"
+    "revision": "1645b9443946df853b25",
+    "url": "/robotFriends/static/js/2.c8745071.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/robotFriends/static/js/2.7e91a854.chunk.js.LICENSE.txt"
+    "url": "/robotFriends/static/js/2.c8745071.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "505c8a500e23440eee91",
-    "url": "/robotFriends/static/js/main.7e20cba0.chunk.js"
+    "revision": "f83495740799f9201a28",
+    "url": "/robotFriends/static/js/main.b163e4b1.chunk.js"
   },
   {
     "revision": "6e28b547d8cfa8bf0f5c",
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundry';
 import './App.css';
 
 
@@ -46,7 +47,10 @@ class App extends Component {
                     <h1 className="text-center">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
+                        
                     </Scroll>
                     
                 </div>
